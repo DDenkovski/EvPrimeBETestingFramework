0b0f704bb169ddb5242ff1595f72dab0b7ed5748
DDenkovski <denisdenkovski0@gmail.com>
2024-10-29 10:39:01 +0100

    first commit

---
+++ src/test/java/EVPrimeTests/UpdateEventTests.java
@@ -0,0 +1,89 @@
+package EVPrimeTests;
+
+import client.EVPrimeClient;
+import data.PostEventDataFactory;
+import data.SignUpLoginDataFactory;
+import database.DbClient;
+import io.restassured.response.Response;
+import jdk.jfr.Description;
+import models.request.PostUpdateEventRequest;
+import models.request.SignUpLoginRequest;
+import models.response.LoginResponse;
+import models.response.PostUpdateDeleteEventResponse;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import util.DateBuilder;
+
+import java.sql.SQLException;
+
+import static objectbuilder.PostEventObjectBuilder.createBodyForPostEvent;
+import static objectbuilder.SignUpObjectBuilder.createBodyForSignUp;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class UpdateEventTests {
+
+    DbClient dbClient = new DbClient();
+    private static String id;
+    private static SignUpLoginRequest signUpRequest;
+    private static LoginResponse loginResponseBody;
+    private static PostUpdateEventRequest requestBody;
+    private static PostUpdateDeleteEventResponse postResponse;
+    private static DateBuilder dateBuilder;
+
+    @BeforeClass
+    public static void setUp() {
+        signUpRequest = new SignUpLoginDataFactory(createBodyForSignUp())
+                .setEmail(RandomStringUtils.randomAlphanumeric(10)+ new DateBuilder().currentTimeMinusOneHour() + "@gmail.com")
+                .setPassword(RandomStringUtils.randomAlphanumeric(10))
+                .createRequest();
+
+        new EVPrimeClient()
+                .signUp(signUpRequest);
+
+        Response loginResponse = new EVPrimeClient()
+                .login(signUpRequest);
+
+        loginResponseBody = loginResponse.body().as(LoginResponse.class);
+
+        requestBody = new PostEventDataFactory(createBodyForPostEvent())
+                .setTitle(RandomStringUtils.randomAlphanumeric(8))
+                .setImage("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goal.com%2Fen-sg%2Fnews%2Fliverpool-vs-manchester-united-lineups-live-updates%2Fbltf4a9e3c54804c6b8&psig=AOvVaw11pYwQiECKpPWu17jL6s6X&ust=1712771074871000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiy883XtYUDFQAAAAAdAAAAABAE")
+                .setDate(dateBuilder.currentTime())
+                .setLocation(RandomStringUtils.randomAlphanumeric(8))
+                .setDescription("The match between the biggest rivals.")
+                .createRequest();
+
+        Response response = new EVPrimeClient()
+                .postEvent(requestBody, loginResponseBody.getToken());
+
+        postResponse = response.body().as(PostUpdateDeleteEventResponse.class);
+        id = postResponse.getMessage().substring(39);
+    }
+
+    @Test
+    @Disabled
+    @Description("BUG-001")
+    public void updateEventTest() throws SQLException {
+        requestBody.setDate("2024-04-08");
+
+        Response updateResponse = new EVPrimeClient()
+                .updateEvent(requestBody, loginResponseBody.getToken(), postResponse.getMessage().substring(39));
+
+        PostUpdateDeleteEventResponse updateResponseBody = updateResponse.body().as(PostUpdateDeleteEventResponse.class);
+
+        assertEquals(201, updateResponse.statusCode());
+        assertTrue(updateResponseBody.getMessage().contains("Successfully updated the event with id: "));
+        assertEquals(requestBody.getDate(), dbClient.getEventFromDB(postResponse.getMessage().substring(39)).getDate());
+
+    }
+
+    @AfterClass
+    public static void deleteEvent() throws SQLException {
+        assertTrue(new DbClient()
+                .isEventDeletedFromDb(id));
+    }
+}
